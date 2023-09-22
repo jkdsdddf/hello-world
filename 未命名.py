81a44286663d1f2f163aa0085236cc81dea2d3cd
jkdsdddf <145204337+jkdsdddf@users.noreply.github.com>
2023-09-22 12:56:14 +0000

    Update 未命名.py

--- 未命名.py
+++ 未命名.py
@@ -1,35 +1,47 @@
+import random
 import time
 
-def introduction():
-    print("Welcome to the Adventure Game!")
-    time.sleep(1)
-    print("You are standing at the entrance of a mysterious cave.")
-    time.sleep(1)
-    print("Your objective is to explore the cave and find the hidden treasure.")
-    time.sleep(1)
+def generate_random_string(length):
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return ''.join(random.choice(letters) for i in range(length))
 
-def choose_path():
-    choice = input("Which path will you choose? (left/right): ")
-    if choice.lower() == "left":
-        print("You take the left path and encounter a ferocious dragon!")
-        time.sleep(1)
-        print("What will you do?")
-        time.sleep(1)
-        # Rest of the scenario and choices
-    elif choice.lower() == "right":
-        print("You take the right path and find a locked door.")
-        time.sleep(1)
-        print("What will you do?")
-        time.sleep(1)
-        # Rest of the scenario and choices
-    else:
-        print("Invalid choice. Please choose again.")
-        choose_path()
+def generate_random_number(digits):
+    return random.randrange(10**(digits-1), 10**digits)
 
-def main():
-    introduction()
-    choose_path()
-    # Rest of the game logic
+def print_progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    if iteration == total: 
+        print()
 
-if __name__ == "__main__":
-    main()
+items = []
+for i in range(5000):
+    items.append({
+        'id': generate_random_number(10),
+        'name': generate_random_string(20),
+        'description': generate_random_string(100),
+        'price': random.random() * 100,
+        'quantity': random.randint(0, 100)
+    })
+
+print("Generating random dataset...")
+for i, item in enumerate(items):
+    print_progress_bar(i + 1, len(items), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    time.sleep(0.01)
+
+print("Dataset generated.")
+
+def bulk_insert(items):
+    print("Inserting items into database...")
+    for i, item in enumerate(items):
+        print_progress_bar(i + 1, len(items), prefix = 'Progress:', suffix = 'Complete', length = 50)
+        time.sleep(0.01)
+        # Database insert code here
+        pass
+    print("Items inserted.")
+
+bulk_insert(items)
+
+print("Done!")
